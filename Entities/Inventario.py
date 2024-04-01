ba8e85e909e010ec65553d0e539edec957b3986f
KEVIN EDUARDO RAMIREZ RAMOS <kevinram00047@gmail.com>
2024-03-31 18:45:15 -0600

    fea: parte de ivan

--- Entities/Inventario.py
+++ Entities/Inventario.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy  # Importar desde flask_sqlalchemy
 from datetime import datetime
 from sqlalchemy import Boolean
+from flask_Login import UserMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # Puedes cambiar la URI según tu base de datos
@@ -87,3 +88,23 @@ class PaqueteItem(db.Model):
     estatus = db.Column(Boolean, default=1)
     usuarioregistro = db.Column(db.Integer)
     fecha_registro = db.Column(db.DateTime, default=datetime.now)
+
+
+class Usuario(db.Model,UserMixin):
+    __tablename__ = 'usuario'
+
+    id_us = db.Column(db.Integer, primary_key=True)  # Auto increment se maneja automáticamente
+    tipousuario = db.Column(db.String(50), nullable=False)
+    nombrecompleto = db.Column(db.String(70), nullable=False)
+    estatus = db.Column(db.String(1), default='1')  # BIT en MySQL se maneja como Boolean en SQLAlchemy
+    usuario_registro = db.Column(db.Integer, nullable=False)
+    fecha_registro = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    ultima_sesion = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    ultima_modificacion = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    failed_attempts = db.Column(db.Integer, default=0)
+
+    def get_id(self):
+        return str(self.id_us)
+    
