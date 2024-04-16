0f18c7dd2af02a4bb21cdee2940b78dc84a4b513
Lalo L <thunderchihuahua2013@gmail.com>
2024-04-15 21:54:06 -0600

    Cambios que me emputan un chingo que pidió el profe alv

--- config.py
+++ config.py
@@ -19,13 +19,22 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Clave_nueva')
     SESSION_COOKIE_SECURE = True
 
+# class DevelopmentConfig(Config):
+#     DEBUG = True
+#     DB_USER = os.environ.get('DB_USER', decrypted_DB_USER)
+#     DB_PASS = os.environ.get('DB_PASS', decrypted_DB_PASS)
+#     DB_HOST = os.environ.get('DB_HOST', decrypted_DB_HOST)
+#     DB_NAME = os.environ.get('DB_NAME', decrypted_DB_NAME)
+    
+#     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{urllib.parse.quote_plus(DB_PASS)}@{DB_HOST}/{DB_NAME}'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 class DevelopmentConfig(Config):
     DEBUG = True
-    DB_USER = os.environ.get('DB_USER', decrypted_DB_USER)
-    DB_PASS = os.environ.get('DB_PASS', decrypted_DB_PASS)
-    DB_HOST = os.environ.get('DB_HOST', decrypted_DB_HOST)
-    DB_NAME = os.environ.get('DB_NAME', decrypted_DB_NAME)
+    DB_USER = os.environ.get('DB_USER', 'root')
+    DB_PASS = os.environ.get('DB_PASS', '1234')
+    DB_HOST = os.environ.get('DB_HOST', 'localhost')
+    DB_NAME = os.environ.get('DB_NAME', 'cookiesIncF')
     
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{urllib.parse.quote_plus(DB_PASS)}@{DB_HOST}/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
